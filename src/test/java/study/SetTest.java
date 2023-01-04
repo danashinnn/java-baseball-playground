6d268dff0f9b7fd18b62e0b2b0d7870cf911adff
danashinnn <103996283+danashinnn@users.noreply.github.com>
2023-01-05 02:52:06 +0900

    학습테스트 복습

--- src/test/java/study/SetTest.java
+++ src/test/java/study/SetTest.java
@@ -3,6 +3,7 @@ package study;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvFileSource;
 import org.junit.jupiter.params.provider.CsvSource;
 import org.junit.jupiter.params.provider.ValueSource;
 
@@ -25,4 +26,22 @@ public class SetTest {
         numbers.add(3);
     }
 
+    @Test
+    void checkSize() {
+        int size = numbers.size();
+        assertThat(size).isEqualTo(3);
+    }
+
+    @ParameterizedTest
+    @ValueSource(ints = {1, 2, 3})
+    void checkContains1(int num) {
+        assertTrue(numbers.contains(num));
+    }
+
+    @ParameterizedTest
+    @CsvSource(value = {"1:true", "2:true", "3:true", "4:false", "5:false"}, delimiter = ':')
+    void checkContains2(int num, boolean expected) {
+        assertThat(numbers.contains(num)).isEqualTo(expected);
+        assertEquals(expected, numbers.contains(num));
+    }
 }
--- src/test/java/study/StringTest.java
+++ src/test/java/study/StringTest.java
@@ -12,4 +12,55 @@ public class StringTest {
         assertThat(actual).isEqualTo("adc");
     }
 
+    @Test
+    void split() {
+        String[] str1 = "1,2".split(",");
+        String[] str2 = "1".split(",");
+
+        assertThat(str1).contains("1", "2");
+        assertThat(str1).containsExactly("1", "2");
+
+        assertThat(str2).contains("1");
+        assertThat(str2).containsExactly("1");
+    }
+
+    @Test
+    void substring() {
+        String str1 = "(1,2)";
+        String str2 = str1.substring(1, str1.length()-1);
+        assertThat(str2).isEqualTo("1,2");
+    }
+
+    @DisplayName("charAt Test")
+    @Test
+    void charAt() {
+        String str = "abc";
+        int idx = 1;
+        char ch = str.charAt(idx);
+        assertThat(ch).isEqualTo('b');
+    }
+
+    @DisplayName("charAt Exception1")
+    @Test
+    void charAtException1() {
+        String str = "abc";
+        int idx = 5;
+        assertThatThrownBy(() -> {
+            char ch = str.charAt(idx);
+            assertThat(ch).isEqualTo('b');
+        }).isInstanceOf(IndexOutOfBoundsException.class)
+                .hasMessageContaining("%d", idx);
+    }
+
+    @DisplayName("charAt Exception2")
+    @Test
+    void charAtException2() {
+        String str = "abc";
+        int idx = 5;
+        assertThatExceptionOfType(IndexOutOfBoundsException.class)
+                .isThrownBy(() -> {
+                    char ch = str.charAt(idx);
+                    assertThat(ch).isEqualTo('b');
+                }).withMessageContaining("%d", idx);
+    }
 }
