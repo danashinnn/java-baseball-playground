e9f00f7533bd113b57b5a31293e01da804b85eee
danashinnn <103996283+danashinnn@users.noreply.github.com>
2023-01-07 03:18:02 +0900

    문자열계산기 테스트 코드 작성

--- src/test/java/study/StringCalculatorTest.java
+++ src/test/java/study/StringCalculatorTest.java
@@ -1,5 +1,49 @@
 package study;
 
-public class StringCalculatorTest {
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 
+import static org.assertj.core.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+class StringCalculatorTest {
+
+    StringCalculator sc;
+
+    @BeforeEach
+    void setUp() {
+        sc = new StringCalculator();
+    }
+
+    @Test
+    public void stringToArrayTest() {
+        String str = "1 + 2";
+        assertThat(sc.stringToArray(str)).containsExactly("1", "+", "2");
+    }
+
+    @Test
+    public void stringToIntTest() {
+        assertEquals(1, sc.stringToInt("1"));
+    }
+
+    @Test
+    public void calculatorTest() {
+        assertEquals(3, sc.calculator(1, "+", 2));
+    }
+
+    @Test
+    public void calculateInOrderTest() {
+        assertEquals(1, sc.calculateInOrder(sc.stringToArray("3 - 2")));
+    }
+
+    @Test
+    public void isBlankTest() {
+        assertTrue(sc.isBlank(" "));
+    }
+
+    @Test
+    public void makeResultTest() {
+        assertEquals(6, sc.calculateInOrder(sc.stringToArray("2 * 3")));
+    }
 }
